f18a912ba32b3c967008eacbe84e424783cecf99
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2025-03-23 21:46:05 -0400

    created Account Toggle + imported it to sidebar

---
+++ src/components/Sidebar/AccountToggle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AccountToggle = () => {
+  return <div>AccountToggle</div>;
+};
+
+export default AccountToggle;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import AccountToggle from "./AccountToggle";
 
 const Sidebar = () => {
   return (
     <div>
       <div className="overflow-y-auto sticky top-4 h-[calc(100dvh-32px-48px)]">
-        {/* TODO: MAIN Sidebar content */}
-        <div className="h-[200dvh]">Content</div>
+        <AccountToggle />
+        {/* <div className="h-[200dvh]">Content</div> */}
       </div>
       {/* TODO: Plan toggle */}
     </div>
