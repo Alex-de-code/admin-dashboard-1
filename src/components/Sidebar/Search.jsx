e3c7b26aa88df720303dc1ea5ca9ad64c71571e2
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2025-03-23 22:40:27 -0400

    imported cmdK to handle search, created commanmenu file w/ logic for search + added search usestate to search file

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import { FiCommand, FiSearch } from "react-icons/fi";
 
 const Search = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
@@ -12,6 +14,8 @@ const Search = () => {
           className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
         />
         <span className="p-1 text-xs flex gap-0.5 items-center shadow bg-stone-50 rounded absolute right-1.5 top-1/2 -translate-y-1/2">
+          {/* will use CmdK npm package to handle search functionality
+          https://www.npmjs.com/package/cmdk */}
           <FiCommand />K
         </span>
       </div>
