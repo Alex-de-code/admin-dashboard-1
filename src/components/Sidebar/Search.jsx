3eef87f6861502ab2b82fb1d00cf4149acc5d7f6
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2025-03-23 22:45:19 -0400

    imported command menu to search + setup OnFOCUS 4 search bar

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { useState } from "react";
 import { FiCommand, FiSearch } from "react-icons/fi";
+import CommandMenu from "./CommandMenu";
 
 const Search = () => {
   const [open, setOpen] = useState(false);
@@ -9,6 +12,10 @@ const Search = () => {
       <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
         <FiSearch className="mr-2" />
         <input
+          onFocus={(e) => {
+            e.target.blur();
+            setOpen(true);
+          }}
           type="text"
           placeholder="Search"
           className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
@@ -19,6 +26,7 @@ const Search = () => {
           <FiCommand />K
         </span>
       </div>
+      <CommandMenu open={open} setOpen={setOpen} />
     </>
   );
 };
