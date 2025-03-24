49b858fcbb2d870a8527281b7f288d7eaaeceb85
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2025-03-23 22:18:50 -0400

    created search file + imported it

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AccountToggle from "./AccountToggle";
+import Search from "./Search";
 
 const Sidebar = () => {
   return (
     <div>
       <div className="overflow-y-auto sticky top-4 h-[calc(100dvh-32px-48px)]">
         <AccountToggle />
+        <Search />
         {/* <div className="h-[200dvh]">Content</div> */}
       </div>
       {/* TODO: Plan toggle */}
